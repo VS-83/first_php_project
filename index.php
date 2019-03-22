caa0c6e4ba8016552f945747f09c87d5e5ea8b61
Vitalikadmin13 <vitaliysonko83.vs@gmail.com>
2019-03-22 21:08:00 +0200

    add new file

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+//$skis = 'Rossignol';
+//echo "My skis are $skis" . PHP_EOL;
+//echo '$skis is a ski company';
+
+//$var = 'Bob';
+//$Var = 'joe';
+//echo  "$var, $Var" . PHP_EOL;
+//
+//;$skiName = 'Blizzard' . PHP_EOL;
+//echo $skiName;
+//$skiName = 'Rossignoll';
+//echo $skiName . PHP_EOL;
+//echo __DIR__;
+
+$alfa = 100 / 100;
+
+$beta = 200;
+
+echo $alfa;
+
+$alfa = $beta;
+
+echo $alfa;
+
+?>

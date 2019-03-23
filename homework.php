50613ade05345452dca0cec130a544675723ec4d
[VS-83] <[vitaliysonko83.vs@gmail.com]>
2019-03-23 22:45:14 +0200

    new-modified

--- homework.php
+++ homework.php
@@ -1,11 +1,29 @@
 <?php
-
 $name = Bob;
 $age = 20;
-
-echo "Hello $name You are $age years old" . PHP_EOL;
-
-echo '<br/>';
-
-echo 'Hello Bob you are 20 years old';
-
+    echo "Hello $name You are $age years old" . PHP_EOL;
+        echo '<br/>';
+    echo 'Hello Bob you are 20 years old';
+        echo '<br/>';
+$x = (int)10;
+$y  = (float)13.5;
+$sum = $x + $y;
+    echo "The sum of $x and $y is $sum";
+      const STRING_SIMPLE = 'This is constant';
+        const IN_DECIMA = 100;
+          const FL_DECIMA = 14.1;
+        echo '<br/>';
+    echo FL_DECIMA;
+        echo '<br/>';
+    echo IN_DECIMA;
+        echo '<br/>';
+    echo STRING_SIMPLE;
+        echo '<br/>';
+$name = 'Vasiliy';
+$admin = $name;
+    echo $admin;
+$third_planet = 'earth';
+$next_vizitor = 'Petya';
+        echo '<br/>';
+//$a = 2;
+//$x = 1 + (a *= 2); //х не будет равен нечему, условие в скобках не выполнится.

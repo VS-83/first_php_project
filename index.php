6d5c67ecbffc768c633f7db6d6e2303e218cd517
[VS-83] <[vitaliysonko83.vs@gmail.com]>
2019-03-23 22:55:23 +0200

    modified index.php

--- index.php
+++ index.php
@@ -3,44 +3,27 @@
 //$skis = 'Rossignol';
 //echo "My skis are $skis" . PHP_EOL;
 //echo '$skis is a ski company';
-
 //$var = 'Bob';
 //$Var = 'joe';
 //echo  "$var, $Var" . PHP_EOL;
-//
 //;$skiName = 'Blizzard' . PHP_EOL;
 //echo $skiName;
 //$skiName = 'Rossignoll';
 //echo $skiName . PHP_EOL;
 //echo __DIR__;
-
 //$alfa = 100 / 100;
-//
 //$beta = 200;
-//
 //echo $alfa;
-//
 //$alfa = $beta;
-
 //echo $alfa;
-
 //$Arr = [1,2,3,4];
-//
 //print_r($Arr);
-//
 //var_dump($Arr);
-//
 //gettype($Arr);
-
-
 //$var_string = ["vitalik", "andrey", "nikita", "abcd"];
-//
-//print_r($var_string);
-//
+//print_r($var_string); //show array for print_r
 //$day_we = ['sun', 'mon', 'tus', 'thu', 'fre', 'sut',];
-//
 //echo $var_string[2];
-//
 //echo $day_we[0];
 
 $next = 0;

2b7cd0e71339b2575289506249b7d30f3cfdd451
Vitaliy Sonko <35668257+VS-83@users.noreply.github.com>
2019-03-24 12:27:38 +0200

    Revert "Feature"

--- index.php
+++ index.php
@@ -3,29 +3,48 @@
 //$skis = 'Rossignol';
 //echo "My skis are $skis" . PHP_EOL;
 //echo '$skis is a ski company';
+
 //$var = 'Bob';
 //$Var = 'joe';
 //echo  "$var, $Var" . PHP_EOL;
+//
 //;$skiName = 'Blizzard' . PHP_EOL;
 //echo $skiName;
 //$skiName = 'Rossignoll';
 //echo $skiName . PHP_EOL;
 //echo __DIR__;
+
 //$alfa = 100 / 100;
+//
 //$beta = 200;
+//
 //echo $alfa;
+//
 //$alfa = $beta;
+
 //echo $alfa;
+
 //$Arr = [1,2,3,4];
+//
 //print_r($Arr);
+//
 //var_dump($Arr);
+//
 //gettype($Arr);
+
+
 //$var_string = ["vitalik", "andrey", "nikita", "abcd"];
-//print_r($var_string); //show array for print_r
+//
+//print_r($var_string);
+//
 //$day_we = ['sun', 'mon', 'tus', 'thu', 'fre', 'sut',];
+//
 //echo $var_string[2];
+//
 //echo $day_we[0];
+
 $next = 0;
+
 if ($next == 0){
     echo "this construction is PHP";
 }
